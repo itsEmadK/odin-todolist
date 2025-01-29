ace2aedfc941fc2667f8ed0e2ecd03a94b473f79
Emad Kheyroddin <67466634+itsEmadK@users.noreply.github.com>
2025-01-29 17:38:17 +0330

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    context: path.resolve(__dirname, "src"), // Set the context to 'src'
+    entry: "./index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|png|jpeg|svg|gif|webm|ttf|otf|woff|woff2)$/i,
+                type: "asset/resource",
+                generator: {
+                    // Specify the output folder and structure
+                    filename: '[path][name][ext]'
+                },
+            },
+        ],
+    },
+
+}
