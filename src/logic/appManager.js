4adf4362567054028eba622692e86ad04f6442bb
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-01-29 20:57:36 +0330

    Add methods for adding a task to a project

--- src/logic/appManager.js
+++ src/logic/appManager.js
@@ -1,3 +1,5 @@
+import { Task } from "./task.js";
+
 const appManager = (function () {
 
     let projects = [];
@@ -21,8 +23,32 @@ const appManager = (function () {
         }
     }
 
+    function createTaskInProject(projectID, taskTitle, taskDesc, taskDueDate, taskPriority) {
+        const newTaskID = generateNextTaskIDForProject(projectID);
+        const task = new Task(
+            newTaskID,
+            taskTitle,
+            taskDesc,
+            taskDueDate, taskPriority
+        );
+        addTaskToProject(projectID, task);
+    }
+
+    function addTaskToProject(projectID, task) {
+        const projectIndex = findProjectIndexByID(projectID);
+        const project = projects[projectIndex];
+        project.addTask(task);
+        projects[projectIndex] = project;
+    }
+
+    function generateNextTaskIDForProject(projectID) {
+        const projectIndex = findProjectIndexByID(projectID);
+        return projects[projectIndex].length;
+    }
+
     return {
         findProjectByID,
+        createTaskInProject,
     };
 })();
 
