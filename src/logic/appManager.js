f293b3063e4fcdc36d19708616d43b8d54558bbc
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-01-29 20:44:46 +0330

    Add method to appmanager to find project index by id

--- src/logic/appManager.js
+++ src/logic/appManager.js
@@ -2,7 +2,15 @@ const appManager = (function () {
 
     let projects = [];
 
-   
+    function findProjectIndexByID(projectID) {
+        for (let i = 0; i < projects.length; i++) {
+            const project = projects[i];
+            if (project.id === projectID) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     return {
 
