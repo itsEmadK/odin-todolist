215049fcfda7e76319e882d764cdbb7e1fafb8d9
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-02-03 16:14:38 +0330

    Fix timeframe changes

--- src/index.js
+++ src/index.js
@@ -2,41 +2,36 @@ import "./styles.css";
 import { appManager } from "./logic/appManager.js";
 import { uiManager } from "./UI/uiManager.js";
 import { TIME_FRAME_VALUES } from "./UI/timeFrameValues.js";
+import { isValid } from "date-fns";
 
-appManager.createProject("P1", "PD1");
-appManager.createProject("P2", "PD2");
-
-appManager.createTaskInProject(0, "T1", "TD1", new Date(2025, 0, 28), 1);
-appManager.createTaskInProject(0, "T2", "TD2", new Date(2025, 1, 3), 2);
-appManager.createTaskInProject(0, "T3", "TD3", new Date(2025, 1, 5), 3);
-appManager.createTaskInProject(1, "T4", "TD4", new Date(2025, 1, 7), 4);
 
 let selectedProjectID = 0;
+let currentTimeFrame = TIME_FRAME_VALUES.NONE;
 
+let addTaskNodeToList = (task) => {
+    uiManager.addTaskNodeToList(
+        task,
+        (newTitle, newDesc, newPriority, newDueDate) => {
+            onTaskEdited(task.id, newTitle, newDesc, newPriority, newDueDate);
+        },
+        () => { onTaskRemoved(task.id) },
+        () => { onTaskFinishedToggled(task.id) }
+    )
+}
 
 let onTaskAdded = (title, desc, priority, dueDate) => {
     const taskID = appManager.createTaskInProject(selectedProjectID, title, desc, new Date(dueDate), +priority);
-    uiManager.addTaskNodeToList(
-        appManager.findTaskByID(selectedProjectID, taskID),
-        (newTitle, newDesc, newPriority, newDueDate) => {
-            onTaskEdited(taskID, newTitle, newDesc, newPriority, newDueDate);
-        },
-        () => {
-            onTaskRemoved(taskID);
-        },
-        () => {
-            onTaskFinishedToggled(taskID);
-        }
-    );
+    addTaskNodeToList(appManager.findTaskByID(taskID));
 }
 
 let onTaskEdited = (taskID, newTitle, newDesc, newPriority, newDueDate) => {
+    const date = new Date(newDueDate);
     appManager.editTaskInProject(
         selectedProjectID, taskID,
         newTitle,
         newDesc,
-        new Date(dueDate),
-        +priority
+        isValid(date) ? date : null,
+        +newPriority
     );
     const editedTask = appManager.findTaskByID(selectedProjectID, taskID)
     uiManager.updateTaskNodeInTheList(editedTask);
@@ -53,18 +48,11 @@ let onTaskFinishedToggled = (taskID) => {
     uiManager.updateTaskNodeInTheList(editedTask);
 }
 
-
 let onTimeFrameChanged = (timeFrame) => {
+    currentTimeFrame = timeFrame;
     uiManager.removeAllTaskNodes();
-    appManager.getProjectTasks(selectedProjectID, timeFrame).forEach(task => {
-        uiManager.addTaskNodeToList(
-            task,
-            (newTitle, newDesc, newPriority, newDueDate) => {
-                onTaskEdited(task.id, newTitle, newDesc, newPriority, newDueDate);
-            },
-            () => { onTaskRemoved(task.id) },
-            () => { onTaskFinishedToggled(task.id) }
-        )
+    appManager.getProjectTasks(selectedProjectID, currentTimeFrame).forEach(task => {
+        addTaskNodeToList(task);
     });
 }
 
@@ -75,17 +63,5 @@ uiManager.init(
     onTimeFrameChanged,
 );
 
-const tasks = appManager.getProjectTasks(selectedProjectID);
 
-for (let i = 0; i < tasks.length; i++) {
-    let task = tasks[i];
-    uiManager.addTaskNodeToList(
-        task,
-        (newTitle, newDesc, newPriority, newDueDate) => {
-            onTaskEdited(task.id, newTitle, newDesc, newPriority, newDueDate);
-        },
-        () => { onTaskRemoved(task.id) },
-        () => { onTaskFinishedToggled(task.id) }
-    )
-
-}
+onTimeFrameChanged(TIME_FRAME_VALUES.NONE)
